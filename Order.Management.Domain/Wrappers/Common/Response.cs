d1c3f454423182bbeb9297b7cd43fa697683042e
rBaun <runeb95@gmail.com>
2021-07-13 15:50:09 +0200

    Added Response and Pagination Wrappers in the Domain

---
+++ Order.Management.Domain/Wrappers/Common/Response.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.Domain.Wrappers.Common
+{
+    public class Response<T>
+    {
+        public T Data { get; set; }
+        public bool Succeeded { get; set; }
+        public string[] Errors { get; set; }
+        public string Message { get; set; }
+
+        public Response() {}
+
+        public Response(T data)
+        {
+            Data = data;
+            Succeeded = true;
+            Errors = null;
+            Message = string.Empty;
+        }
+    }
+}
---
+++ Order.Management.Domain/Wrappers/Pagination/PaginationFilter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.Domain.Wrappers.Pagination
+{
+    public class PaginationFilter
+    {
+        private const int DefaultPageNumber = 1;
+        private const int DefaultPageSize = 10;
+        private const int PageSizeCap = 50;
+
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public string SearchTerm { get; set; }
+        public string OrderBy { get; set; }
+
+        public PaginationFilter()
+        {
+            PageNumber = DefaultPageNumber;
+            PageSize = DefaultPageSize;
+        }
+
+        public PaginationFilter(int pageNumber, int pageSize)
+        {
+            PageNumber = SetPageNumber(pageNumber);
+            PageSize = SetPageSize(pageSize);
+        }
+
+        public PaginationFilter(int pageNumber, int pageSize, string searchTerm, string orderBy)
+        {
+            PageNumber = SetPageNumber(pageNumber);
+            PageSize = SetPageSize(pageSize);
+            SearchTerm = searchTerm;
+            OrderBy = orderBy;
+        }
+
+        private static int SetPageNumber(int pageNumber)
+        {
+            return pageNumber < 1 ? 1 : pageNumber;
+        }
+
+        private static int SetPageSize(int pageSize)
+        {
+            return pageSize > PageSizeCap || pageSize == 0 ? PageSizeCap : pageSize;
+        }
+    }
+}
